8aa5970170d4d3cbf255cda49fe5edccbfcd21df
Edmy20 <74212389+Edmy20@users.noreply.github.com>
2021-04-20 14:22:02 -0600

    eer
    no terminal 2do

--- automata_pila.py
+++ automata_pila.py
@@ -8,7 +8,7 @@ def seleccionar_gramatica():
         print(gra.nombre)
 
     print("Buscar nombre")
-    nombre = "Gramatica1"
+    nombre = "Grm1"
 
     for g in gramaticas:
         if g.nombre == nombre:
@@ -29,15 +29,17 @@ def imprimir_pila(pila,i,entrada):
 #----------------------------------------------------------
 def pda():
     gramatica = seleccionar_gramatica()
-    entrada = "zazabzbz"
+    #entrada = "zazabzbz"
+    entrada = "abzba"
     gramatica.imprimir_gramatica()
     length = len(entrada)
     state = "i"
     pila = []
+    error = False
     i=0
     
-    while (i<length):
-        currentChart = entrada[i]
+    while (i<=length):
+
         if state=="i":
             valido = simbolo("#","NO")
             pila.append(valido)
@@ -54,8 +56,7 @@ def pda():
 
         elif state == "q":
             for produccion in gramatica.producciones:
-                cima = pila[-1]
-                if cima.terminal=="NO" and cima.sim==produccion.produc_nombre:
+                if pila[-1].terminal=="NO" and pila[-1].sim==produccion.produc_nombre:
                     if len(produccion.rules)==1:
                         pila.pop()
                         imprimir_pila(pila,i,entrada)
@@ -68,25 +69,53 @@ def pda():
                             elem.reverse()
                     else:
                         #regla = None
-                        n=0
+                        cambiado = False
+
                         for rule in produccion.rules:
-                            n+=1
-                            
+
                             if rule[0].sim == entrada[i]:
-                                #print("---------")
-                                #print(currentChart)
+                                cambiado = True
                                 pila.pop()
                                 rule.reverse()
                                 for el in rule:
                                     pila.append(el)
                                 imprimir_pila(pila,i,entrada)
-                                #print(n)
                                 rule.reverse()
+                        if cambiado == False:
+                                for rule in produccion.rules:
+                                    if rule[0].terminal == "NO":
+                                        cambiado == True
+                                        pila.pop()
+                                        rule.reverse()
+                                        for el in rule:
+                                            pila.append(el)
+                                        print("####")
+                                        imprimir_pila(pila,i,entrada)
+                                        rule.reverse()
 
-                elif cima.terminal=="SI" and cima.sim == entrada[i]:
+
+                elif pila[-1].terminal=="SI" and pila[-1].sim == entrada[i]:
                     i+=1
+                    print(i)
                     pila.pop()
                     imprimir_pila(pila,i,entrada)
+                elif pila[-1].terminal=="SI" and pila[-1].terminal != entrada[i]:
+                    error = True
+                    print("Error")
+                    state = "f"
+                    break
+                elif len(pila)==1 and pila[0].sim=="#":
+                    pila.pop()
+                    state = "f"
+                    break
+        elif state =="f":
+            print(i)
+            if i==length and error==False:
+                print(entrada+" es una palabra admitida")
+            else:
+                print("Error: "+entrada+" No es una palabra admitida")
+            
+            i=length+2
 
                         
 
